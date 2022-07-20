1c4060f5f42f06a197e4e4327bef48856d789628
Humam Abo Alraja <humamaboalraja@gmail.com>
2022-07-21 00:01:05 +0200

    doc: Fix `top k frequent elements` row details

--- coding/solutions/src/main/java/xyz/grind/coding/blind75/TopKFrequentElements.java
+++ coding/solutions/src/main/java/xyz/grind/coding/blind75/TopKFrequentElements.java
@@ -1,6 +1,7 @@
 package xyz.grind.coding.blind75;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,32 +29,24 @@ public class TopKFrequentElements {
    }
 
    public static int[] topKFrequentElementsHeap(int[] nums, int k){
-      // O(1) time
       if (k == nums.length) {
          return nums;
       }
 
-      // 1. build hash map : character and how often it appears
-      // O(N) time
       Map<Integer, Integer> count = new HashMap();
       for (int n: nums) {
          count.put(n, count.getOrDefault(n, 0) + 1);
       }
 
-      // init heap 'the less frequent element first'
+      // when initializing the heap we keep the less frequent element first
       Queue<Integer> heap = new PriorityQueue<>(
               (n1, n2) -> count.get(n1) - count.get(n2));
 
-      // 2. keep k top frequent elements in the heap
-      // O(N log k) < O(N log N) time
       for (int n: count.keySet()) {
-         System.out.println("keyset " + n);
          heap.add(n);
          if (heap.size() > k) heap.poll();
       }
 
-      // 3. build an output array
-      // O(k log k) time
       int[] top = new int[k];
       for(int i = k - 1; i >= 0; --i) {
          top[i] = heap.poll();
