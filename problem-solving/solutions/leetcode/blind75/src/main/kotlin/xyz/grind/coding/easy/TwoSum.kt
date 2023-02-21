8272eb17c146662b5695f5f01cf77ef4c86e8813
humamaboalraja <humamaboalraja@gmail.com>
2023-02-21 10:21:49 +0100

    ref(two-sum): Clean up Kotlin Solution and tests

--- problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/TwoSum.kt
+++ problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/TwoSum.kt
@@ -1,3 +1,36 @@
 package xyz.grind.coding.easy
 
-object TwoSum
+object TwoSum {
+
+    fun twoSumBruteForce(nums: IntArray, target: Int): IntArray {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[i] + nums[j] == target) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+
+    fun twoSumBruteForceTwo(nums: IntArray, target: Int): IntArray {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[j] == target - nums[i]) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) return intArrayOf(map[complement]!!, i)
+            map[nums[i]] = i
+        }
+        return intArrayOf(0)
+    }
+}
--- problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/TwoSumTest.kt
+++ problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/TwoSumTest.kt
@@ -1,3 +1,58 @@
 package easy
 
-class TwoSumTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+import xyz.grind.coding.easy.TwoSum
+
+class TwoSumTest {
+    @Test
+    fun `Should return array that contains two elements that sum up to target uf found these elements`() {
+        Assertions.assertAll(
+            Executable {
+                assertArrayEquals(TwoSum.twoSumBruteForce(intArrayOf(20, 2, 3, 8, 5, 3), 28), intArrayOf(0, 3))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSumBruteForce(intArrayOf(2, 7, 11, 15), 9), intArrayOf(0, 1))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSumBruteForceTwo(intArrayOf(3, 2, 4), 6), intArrayOf(1, 2))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSumBruteForceTwo(intArrayOf(3, 1, 20, 89), 90), intArrayOf(1, 3))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSum(intArrayOf(20, 2, 3, 8, 5, 3), 28), intArrayOf(0, 3))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSum(intArrayOf(2, 7, 11, 15), 9), intArrayOf(0, 1))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSum(intArrayOf(3, 2, 4), 6), intArrayOf(1, 2))
+            },
+            Executable {
+                assertArrayEquals(TwoSum.twoSum(intArrayOf(3, 1, 20, 60), 80), intArrayOf(2, 3))
+            }
+        )
+    }
+
+    @Test
+    fun `Should return zero if no the array doesn't contain two numbers that sum up to target`() {
+        Assertions.assertAll(
+            Executable {
+                assertEquals(TwoSum.twoSum(intArrayOf(20, 2, 3, 8, 5, 3), 1324)[0], 0)
+            },
+            Executable {
+                assertEquals(TwoSum.twoSum(intArrayOf(2, 7, 11, 15), 4332)[0], 0)
+            },
+            Executable {
+                assertEquals(TwoSum.twoSum(intArrayOf(3, 2, 4), 87)[0], 0)
+            },
+            Executable {
+                assertEquals(TwoSum.twoSum(intArrayOf(3, 1, 20, 89), 2434)[0], 0)
+            }
+        )
+    }
+}
