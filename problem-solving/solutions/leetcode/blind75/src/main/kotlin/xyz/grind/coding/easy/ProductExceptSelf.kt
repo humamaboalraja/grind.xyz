3af805d1bf026b966f4cfdd271eccdb4edc95f01
humamaboalraja <humamaboalraja@gmail.com>
2023-02-22 12:30:40 +0100

    ref(product-array-except-self): Add a new approach

--- problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/ProductExceptSelf.kt
+++ problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/ProductExceptSelf.kt
@@ -4,7 +4,7 @@ object ProductExceptSelf {
     fun productOfArrayExceptSelfBruteForce(nums: IntArray): IntArray {
         var sumOfProducts = 1
         for (num in nums) {
-            sumOfProducts = sumOfProducts * num
+            sumOfProducts *= num
         }
         val answer = IntArray(nums.size)
         for (i in nums.indices) {
@@ -22,29 +22,48 @@ object ProductExceptSelf {
         for (i in 1 until length) {
             leftProduct[i] = nums[i - 1] * leftProduct[i - 1]
         }
+
         rightProduct[length - 1] = 1
         for (i in length - 2 downTo 0) {
             rightProduct[i] = nums[i + 1] * rightProduct[i + 1]
         }
+
         for (i in 0 until length) {
             answer[i] = leftProduct[i] * rightProduct[i]
         }
         return answer
     }
 
-
     fun productExceptSelf(nums: IntArray): IntArray {
-        val length = nums.size
-        val answer = IntArray(length)
+        val answer = IntArray(nums.size)
         answer[0] = 1
-        for (i in 1 until length) {
+        for (i in 1 until nums.size) {
             answer[i] = nums[i - 1] * answer[i - 1]
         }
         var rightProduct = 1
-        for (i in length - 1 downTo 0) {
-            answer[i] = answer[i] * rightProduct
+        for (i in nums.size - 1 downTo 0) {
+            answer[i] *= rightProduct
             rightProduct *= nums[i]
         }
         return answer
+    }
+
+    fun productExceptSelfTwo(nums: IntArray): IntArray {
+        val result = IntArray(nums.size)
+
+        var prefix = 1
+
+        for (i in nums.indices) {
+            result[i] = prefix
+            prefix *= nums[i]
+        }
+
+        var postfix = 1
+        for (i in nums.size - 1 downTo 0) {
+            result[i] *= postfix
+            postfix *= nums[i]
+        }
+
+        return result
     }
 }
--- problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/ProductExceptSelfTest.kt
+++ problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/ProductExceptSelfTest.kt
@@ -8,7 +8,6 @@ import xyz.grind.coding.easy.ProductExceptSelf
 
 class ProductExceptSelfTest {
 
-
     @Test
     fun `Should return return an array answer such that answer'i' is equal to the product of all the elements of nums except nums'i'`() {
         Assertions.assertAll(
@@ -19,7 +18,11 @@ class ProductExceptSelfTest {
             Executable { assertArrayEquals(ProductExceptSelf.productExceptSelf(intArrayOf(2, 3, 4, 5, 6)), intArrayOf(360, 240, 180, 144, 120)) },
             Executable { assertArrayEquals(ProductExceptSelf.productExceptSelf(intArrayOf(3493, 3124, 1234, 987)), intArrayOf(-490066504, -40640002, -2114627604, 580669000)) },
             Executable { assertArrayEquals(ProductExceptSelf.productExceptSelf(intArrayOf(3493, 324, 1234, 987, -1, -9)), intArrayOf(-743401768, -365760018, 1463240764, -315886296, -1752838456, 282458760)) },
-            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelf(intArrayOf(-1, 1, 0, -5, 5)), intArrayOf(0, 0, 25, 0, 0)) }
+            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelf(intArrayOf(-1, 1, 0, -5, 5)), intArrayOf(0, 0, 25, 0, 0)) },
+            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelfTwo(intArrayOf(2, 3, 4, 5, 6)), intArrayOf(360, 240, 180, 144, 120)) },
+            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelfTwo(intArrayOf(3493, 3124, 1234, 987)), intArrayOf(-490066504, -40640002, -2114627604, 580669000)) },
+            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelfTwo(intArrayOf(3493, 324, 1234, 987, -1, -9)), intArrayOf(-743401768, -365760018, 1463240764, -315886296, -1752838456, 282458760)) },
+            Executable { assertArrayEquals(ProductExceptSelf.productExceptSelfTwo(intArrayOf(-1, 1, 0, -5, 5)), intArrayOf(0, 0, 25, 0, 0)) }
         )
     }
 }
