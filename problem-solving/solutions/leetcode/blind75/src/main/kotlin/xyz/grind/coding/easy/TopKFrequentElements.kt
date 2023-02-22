af86c5cf5090dfda56b082140fd1fc3cb1fa7c0d
humamaboalraja <humamaboalraja@gmail.com>
2023-02-22 12:29:27 +0100

    ref(top-k-frequent): Clean up Kotlin Solution and tests

--- problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/TopKFrequentElements.kt
+++ problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/TopKFrequentElements.kt
@@ -1,3 +1,49 @@
 package xyz.grind.coding.easy
 
-object TopKFrequentElements
+import java.util.Queue
+import java.util.PriorityQueue
+import kotlin.collections.HashMap
+import kotlin.collections.MutableList
+import kotlin.collections.MutableMap
+import kotlin.collections.set
+
+object TopKFrequentElements {
+
+    fun topKFrequentElementsHeap(nums: IntArray, k: Int): IntArray {
+        if (k == nums.size) {
+            return nums
+        }
+        val count: MutableMap<Int, Int> = HashMap()
+        for (n in nums) { count[n] = count.getOrDefault(n, 0) + 1 }
+
+        // when initializing the heap we keep the less frequent element first
+        val heap: Queue<Int> = PriorityQueue { n1: Int, n2: Int -> count[n1]!! - count[n2]!! }
+        for (n in count.keys) {
+            heap.add(n)
+            if (heap.size > k) heap.poll()
+        }
+        val top = IntArray(k)
+        for (i in k - 1 downTo 0) top[i] = heap.poll()
+        return top
+    }
+
+
+    fun topKFrequentElements(nums: IntArray, k: Int): IntArray {
+        val count = hashMapOf<Int, Int>()
+        val freq = MutableList<MutableList<Int>>(nums.size + 1) { mutableListOf() }
+        for (n in nums) count[n] = count.getOrDefault(n, 0) + 1
+        for ((n, c) in count) { freq[c].add(n) }
+
+        val res = mutableListOf<Int>()
+        for (i in freq.size - 1 downTo 0) {
+            for (n in freq[i]) {
+                res.add(n)
+                if (res.size == k) return res.toIntArray()
+            }
+        }
+        return intArrayOf()
+    }
+
+
+
+}
--- problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/TopKFrequentElementsTest.kt
+++ problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/TopKFrequentElementsTest.kt
@@ -1,3 +1,104 @@
 package easy
 
-class TopKFrequentElementsTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+import xyz.grind.coding.easy.TopKFrequentElements
+
+class TopKFrequentElementsTest {
+    @Test
+    fun `Should return the k most frequent elements`() {
+        Assertions.assertAll(
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(
+                        intArrayOf(1, 1, 1, 2, 2, 3, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10),
+                        4), intArrayOf(10, 9, 1, 2)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(
+                        intArrayOf(98798, 34243),
+                        2
+                    ), intArrayOf(98798, 34243)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(
+                        intArrayOf(1, 1, 1, 2, 3, 9, 11, 11),
+                        2
+                    ), intArrayOf(1, 11)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(
+                        intArrayOf(1, 1, 1, 2, 3, 9, 11, 11), 2
+                    ), intArrayOf(1, 11)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(
+                        intArrayOf(1, 1, 1, 2, 2, 3, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10), 4),
+                        intArrayOf(10, 9, 1, 2)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(
+                        intArrayOf(98798, 34243), 2), intArrayOf(34243, 98798)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(intArrayOf(9876543), 1), intArrayOf(9876543)
+                )
+            }
+        )
+    }
+
+    @Test
+    fun `Should return the k most frequent elements (edge cases)`() {
+        Assertions.assertAll(
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(intArrayOf(4), 1), intArrayOf(4))
+
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(intArrayOf(98798, 34243), 2), intArrayOf(98798, 34243))
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElementsHeap(
+                        intArrayOf(1, 1, 1, 2, 3, 9, 11, 11), 2), intArrayOf(1, 11))
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(
+                        intArrayOf(11), 1), intArrayOf(11))
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(
+                        intArrayOf(1, 1, 1, 2, 2, 3, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10), 4
+                    ), intArrayOf(10, 9, 1, 2)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(intArrayOf(98798, 34243), 2), intArrayOf(34243, 98798)
+                )
+            },
+            Executable {
+                assertArrayEquals(
+                    TopKFrequentElements.topKFrequentElements(intArrayOf(9876543), 1), intArrayOf(9876543))
+            }
+        )
+    }
+}
