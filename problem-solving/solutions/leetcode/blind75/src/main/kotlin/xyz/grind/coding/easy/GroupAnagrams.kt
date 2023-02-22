cff8d72b911deae491ab4f6776ad98b5aa58935a
humamaboalraja <humamaboalraja@gmail.com>
2023-02-22 04:45:29 +0100

    ref(group-anagrams): Clean up Kotlin Solution and tests

--- problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/GroupAnagrams.kt
+++ problem-solving/solutions/leetcode/blind75/src/main/kotlin/xyz/grind/coding/easy/GroupAnagrams.kt
@@ -1,3 +1,52 @@
 package xyz.grind.coding.easy
 
-object GroupAnagrams
+import java.util.Arrays
+
+object GroupAnagrams {
+
+    fun groupAnagramsBruteForce(strs: Array<String>): List<List<String>> {
+        if (strs.isEmpty()) return ArrayList()
+        val groupedAnagrams: MutableList<List<String>> = ArrayList()
+        val computedAnagrams: MutableMap<String, MutableList<String>> = HashMap()
+        for (str in strs) {
+            val strCharArray = str.toCharArray() // because strings in java are immutable
+            Arrays.sort(strCharArray)
+            val anagramKey = String(strCharArray)
+            if (!computedAnagrams.containsKey(anagramKey)) computedAnagrams[anagramKey] = arrayListOf()
+            computedAnagrams[anagramKey]?.add(str)
+        }
+        groupedAnagrams.addAll(computedAnagrams.values)
+        return groupedAnagrams
+    }
+
+    fun groupAnagramsBruteForceTwo(strs: Array<String>): List<List<String>> {
+        if (strs.isEmpty()) return ArrayList()
+        val computedAnagrams: MutableMap<String, MutableList<String>> = HashMap()
+        for (str in strs) {
+            val strCharArray = str.toCharArray() // because strings in java are immutable
+            Arrays.sort(strCharArray)
+            val anagramKey = String(strCharArray)
+            if (!computedAnagrams.containsKey(anagramKey)) computedAnagrams[anagramKey] = ArrayList()
+            computedAnagrams[anagramKey]!!.add(str)
+        }
+        return ArrayList(computedAnagrams.values)
+    }
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        if (strs.isEmpty()) return ArrayList()
+        val groupedAnagrams: MutableList<List<String>> = ArrayList()
+        val computedAnagram: MutableMap<String, MutableList<String>> = HashMap()
+        for (arrayAnagramElement in strs) {
+            val strCounterCharArray = CharArray(26)
+            for (character in arrayAnagramElement.toCharArray()) {
+                strCounterCharArray[character.code - 'a'.code]++
+            }
+            val keyAnagram = String(strCounterCharArray)
+            if (!computedAnagram.containsKey(keyAnagram)) computedAnagram.computeIfAbsent(keyAnagram) { ArrayList() }
+            computedAnagram[keyAnagram]?.add(arrayAnagramElement)
+            println(computedAnagram)
+        }
+        groupedAnagrams.addAll(computedAnagram.values)
+        return groupedAnagrams
+    }
+}
--- problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/GroupAnagramsTest.kt
+++ problem-solving/solutions/leetcode/blind75/src/test/kotlin/easy/GroupAnagramsTest.kt
@@ -1,3 +1,73 @@
 package easy
 
-class GroupAnagramsTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
+import xyz.grind.coding.easy.GroupAnagrams
+import xyz.grind.coding.easy.ValidAnagram
+
+class GroupAnagramsTest {
+
+    private fun listOfListsInputMock(testCasesRenderer: Boolean): List<List<String>>? {
+        val listOfLists: MutableList<List<String>> = ArrayList()
+        val firstInnerList: MutableList<String> = ArrayList()
+        val secondInnerList: MutableList<String> = ArrayList()
+        val thirdInnerList: MutableList<String> = ArrayList()
+        firstInnerList.add("eat")
+        firstInnerList.add("ate")
+        firstInnerList.add("tea")
+        firstInnerList.add("eta")
+        listOfLists.add(firstInnerList)
+        if (testCasesRenderer) {
+            secondInnerList.add("tba")
+            secondInnerList.add("tab")
+            secondInnerList.add("bat")
+            listOfLists.add(secondInnerList)
+        }
+        thirdInnerList.add("post")
+        thirdInnerList.add("pots")
+        thirdInnerList.add("spot")
+        thirdInnerList.add("stop")
+        thirdInnerList.add("tops")
+        listOfLists.add(thirdInnerList)
+        return listOfLists
+    }
+
+    @Test
+    fun `Should return list of lists that has grouped anagrams that are provided from the strs input array`() {
+        Assertions.assertAll(
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForce(arrayOf("eat", "ate", "tea", "eta", "post", "pots", "spot", "stop", "tops")), listOfListsInputMock(false))
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForce(arrayOf("eat", "ate", "tea", "eta", "tba", "tab", "bat", "post", "pots", "spot", "stop", "tops")), listOfListsInputMock(true))
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForceTwo(arrayOf("eat", "ate", "tea", "eta", "post", "pots", "spot", "stop", "tops")), listOfListsInputMock(false))
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForceTwo(arrayOf("eat", "ate", "tea", "eta", "tba", "tab", "bat", "post", "pots", "spot", "stop", "tops")), listOfListsInputMock(true))
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagrams(arrayOf("eat", "ate", "tea", "eta", "post", "pots", "spot", "stop", "tops")), listOfListsInputMock(false))
+            }
+        )
+    }
+
+    @Test
+    fun `Should return an empty list`() {
+        Assertions.assertAll(
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForce(arrayOf()), ArrayList<String>())
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagramsBruteForceTwo(arrayOf()), ArrayList<String>())
+            },
+            Executable {
+                assertEquals(GroupAnagrams.groupAnagrams(arrayOf()), ArrayList<String>())
+            }
+        )
+    }
+
+}
